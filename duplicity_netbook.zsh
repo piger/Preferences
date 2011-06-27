7eeb70e9ebdf5d9be59490328fc3e84563a074e5
sand <daniel@spatof.org>
2011-06-27 21:27:31 +0200

    update backup netbook

--- duplicity_netbook.zsh
+++ duplicity_netbook.zsh
@@ -1,23 +1,38 @@
 #!/usr/bin/env zsh
 
 # Backup per la home del netbook aziendale.
+#     --gpg-options "--compress-algo=bzip2" \
 
-duplicity \
-	--exclude /home/sand/.toast \
-	--exclude /home/sand/.cache \
-	--exclude /home/sand/.thumbnails \
-	--exclude /home/sand/.ccache \
-	--exclude "/home/sand/VirtualBox VMs" \
-	--exclude /home/sand/dev/pyvuln/env.old \
-	--exclude "/home/sand/dev/**/env/" \
-	--exclude "/home/sand/**/*.pyc" \
-	--exclude /home/sand/tools/msf3 \
-	--exclude /home/sand/tools/msf3_3.5.2 \
-	--exclude /home/sand/Downloads \
-	--name home_daily \
-	--encrypt-key D3B75D54 \
-	--sign-key D3B75D54 \
-	-v 6 \
-	--gpg-options "--compress-algo=bzip2" \
-	/home/sand \
-	scp://sand@taleggio/backup/hal/
+set -e
+
+do_backup() {
+    duplicity \
+        --exclude-globbing-filelist ~/tuttedir.txt \
+        --name home_daily \
+        --encrypt-key D3B75D54 \
+        --sign-key D3B75D54 \
+        -v 6 \
+        /home/sand \
+        scp://sand@taleggio/backup/hal/
+}
+
+do_status() {
+    duplicity \
+        collection-status \
+        --name home_daily \
+        --encrypt-key D3B75D54 \
+        --sign-key D3B75D54 \
+        -v 6 \
+        scp://sand@taleggio/backup/hal/
+}
+
+do_gpg() {
+    duplicity \
+        restore \
+        --name home_daily_gpg \
+        -v 6 \
+        scp://sand@taleggio/backup/hal_gpg \
+        ~/oompa
+}
+
+do_gpg
