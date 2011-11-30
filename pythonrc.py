b64d227817cd6b176cee1af11740b828c1aa87e9
sand <daniel@spatof.org>
2011-11-30 16:50:11 +0100

    fixato ~/.pythonrc.py finalmente

--- pythonrc.py
+++ pythonrc.py
@@ -1,32 +1,4 @@
-# vim: ft=python
-
-# Questo non funzizona su Snow Leopard
-#import readline, rlcompleter
-#readline.parse_and_bind("tab: complete")
-
-# questo funziona su Snow Leopard:
-# http://stackoverflow.com/questions/675370/tab-completion-in-python-interpreter-in-os-x-terminal
-# import rlcompleter
-# import readline
-# readline.parse_and_bind ("bind ^I rl_complete")
-
-# import sys
-
-# try:
-#     import readline
-#     import rlcompleter
-
-#     if sys.platform.startswith('darwin'):
-#         readline.parse_and_bind ("bind ^I rl_complete")
-#     elif sys.platform.startswith('linux'):
-#         readline.parse_and_bind("tab: complete")
-#         # readline.parse_and_bind ("bind ^I rl_complete")
-
-# except ImportError:
-#     pass
-
-
-
+# -*- encoding: utf-8 -*-
 import atexit
 import os.path
 import sys
@@ -65,3 +37,15 @@ if os.path.isfile(history_path):
     readline.read_history_file(history_path)
 readline.set_history_length(100)
 atexit.register(lambda x=history_path: readline.write_history_file(x))
+
+# import readline
+# import rlcompleter
+# 
+# if 'libedit' in readline.__doc__:
+#     readline.parse_and_bind("bind ^I rl_complete")
+# else:
+#     readline.parse_and_bind("tab: complete")
+# 
+# print "hello world"
+
+# vim: ft=python
