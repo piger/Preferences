9479b50cedd97a6319d034fe8f7d2925eaaf23a6
Daniel Kertesz <daniel@spatof.org>
2014-10-24 19:48:17 +0200

    emacs: provo a usare helm

--- elisp-init/init-config.el
+++ elisp-init/init-config.el
@@ -161,6 +161,11 @@
 ;; the blinking cursor is nothing, but an annoyance
 (blink-cursor-mode -1)
 
+;; helm
+(require 'helm-config)
+(global-set-key (kbd "C-c h") 'helm-mini)
+(helm-mode 1)
+(diminish 'helm-mode)
 
 ;; wcheck / aspell / hunspell
 (setq wcheck-language-data
