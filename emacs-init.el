e764bfdd8dce63f859655ad646de7d8e70d1deac
Daniel Kertesz <daniel@spatof.org>
2014-11-10 00:17:23 +0100

    switch to single file emacs settings

--- emacs-init.el
+++ emacs-init.el
@@ -1,50 +1,19 @@
-;;; piger-emacs -- Summary
+;;; new emacs settings using org mode
 
-;; emacs.d/init.el
-;;
-;; - https://github.com/magnars/.emacs.d/blob/master/init.el
-;; - http://www.emacswiki.org/emacs/ImenuMode
+(defconst emacs-start-time (current-time)
+  "This variable hold the time emacs was started.")
 
-;; This file is not part of GNU Emacs.
-
-;;; Commentary:
-
-;; This file simply sets up the default load path and requires
-;; the various modules.
-
-;;; Code:
-;; nasconde la toolbar; e' bene farlo all'inizio per evitare che venga mostrata brevemente
-(when (fboundp 'tool-bar-mode)
-	(tool-bar-mode -1))
-;; nasconde la scroll bar (perchè mai?)
-; (scroll-bar-mode -1)
- 
-(defconst *is-a-mac* (eq system-type 'darwin))
-
-(defvar piger/preferences-dir (expand-file-name "~/Preferences/elisp-init")
-  "The directory containing my elisp files.")
-
-
-;; (add-to-list 'load-path
-;;              (expand-file-name "elisp-init" user-emacs-directory))
-(add-to-list 'load-path piger/preferences-dir)
-
-(require 'init-packages)
-(require 'init-functions)
-(require 'init-bindings)
-(require 'init-config)
-(require 'init-extra-elisp)
-(require 'init-modes)
-(require 'init-aliases)
-(when *is-a-mac*
-  (require 'init-osx))
-
-(let ((piger/local-config
-       (concat (file-name-as-directory piger/preferences-dir) "init-local.el")))
-  (load piger/local-config 'noerror))
-
-;;; emacs custom settings
+;; load custom settings
 (setq custom-file (expand-file-name "custom.el" user-emacs-directory))
-(load custom-file 'noerror)
+(when (file-exists-p custom-file)
+  (load custom-file))
 
-;;; emacs-init.el ends here
+;; load settings file
+(require 'org)
+(org-babel-load-file
+ (expand-file-name "~/Dropbox/org/emacs-settings.org"))
+
+;; display loading time
+(let ((elapsed (float-time (time-subtract (current-time)
+                                          emacs-start-time))))
+  (message "Settings loaded in %.3fs" elapsed))
