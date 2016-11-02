84b8261a39d74e7a9302fed9f249186248493955
Daniel Kertesz <dkertesz@zendesk.com>
2016-11-02 15:02:20 +0000

    emacs: proper initialization of org-mode from elpa (instead of using the built-in one)

--- emacs-init.el
+++ emacs-init.el
@@ -8,12 +8,15 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
+;; init package system to load the updated org-mode package from melpa (or is it from elpa??)
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(package-initialize)
+
 ;; load settings file
-(require 'org)
-(org-babel-load-file
- (expand-file-name "~/Preferences/emacs-settings.org"))
+(org-babel-load-file (expand-file-name "~/Preferences/emacs-settings.org"))
 
 ;; display loading time
-(let ((elapsed (float-time (time-subtract (current-time)
-                                          emacs-start-time))))
+(let ((elapsed (float-time (time-subtract (current-time) emacs-start-time))))
   (message "Settings loaded in %.3fs" elapsed))
