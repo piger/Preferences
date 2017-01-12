d844e229decf9acf7bd18095d11c8b7f0c42e00f
Daniel Kertesz <dkertesz@zendesk.com>
2017-01-12 10:56:09 +0000

    fancier python interactive prompt

--- pythonrc.py
+++ pythonrc.py
@@ -19,3 +19,12 @@ try:
     from see import see
 except ImportError:
     pass
+
+# fancy prompt and pretty print
+import pprint
+import sys
+
+sys.ps1 = "\033[0;34m>>> \033[0m"
+sys.ps2 = "\033[1;34m... \033[0m"
+
+sys.displayhook = pprint.pprint
