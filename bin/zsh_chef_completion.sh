694a67f98eaf21dc6dff7f034fb52f8b7c42c4cb
Daniel Kertesz <daniel@spatof.org>
2017-05-10 02:09:54 +0100

    update zsh completion script for chef

--- bin/zsh_chef_completion.sh
+++ bin/zsh_chef_completion.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env zsh
 # Refresh zsh hostname completion
-{ knife node list; jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json } > ~/.chef_hosts.new && mv ~/.chef_hosts.new ~/.chef_hosts
+#
+# NOTE: requires 'moreutils'
+{ knife node list && jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json } | ifne tee ~/.chef_hosts.new > /dev/null
+[[ -s ~/.chef_hosts.new ]] && mv ~/.chef_hosts.new ~/.chef_hosts
