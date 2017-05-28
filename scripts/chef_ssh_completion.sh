42f027d13413d18d922e7b2fae71cb62a997cd14
Daniel Kertesz <daniel@spatof.org>
2017-05-28 20:40:38 +0100

    more unshit

--- scripts/chef_ssh_completion.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# This script will populate $HOME/.chef_hosts with a list of all servers known to chef, for zsh completion.
-
-find $HOME/src/chef-repo-nodes -name '*.json' | xargs cat | jq -r '.name' > $HOME/.chef_hosts.new && mv $HOME/.chef_hosts.new $HOME/.chef_hosts
