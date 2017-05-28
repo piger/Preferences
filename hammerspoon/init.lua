075e1774c4df1e8206180c8bf0ee1a0bb2405220
Daniel Kertesz <daniel@spatof.org>
2017-05-28 12:32:51 +0100

    hammerspoon: cheatsheet and disable stuff

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -197,11 +197,11 @@ hs.audiodevice.watcher.start()
 setToggleAudioIcon()
 
 --- external modules
-require("weather")
+-- require("weather")
 -- require("music_menu")
 require("cheatsheet")
 -- require("calendar")
-require("expose")
+-- require("expose")
 
 -- reload hammerspoon (and stop all the running watchers) --
 ------------------------------------------------------------
