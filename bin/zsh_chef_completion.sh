cd3b528e12c91ed519bb80e2f5a7a87f553de0e5
Daniel Kertesz <daniel@spatof.org>
2017-06-04 13:40:29 +0100

    add timeout to chef completion script

--- bin/zsh_chef_completion.sh
+++ bin/zsh_chef_completion.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env zsh
 # Refresh zsh hostname completion
-#
-# NOTE: requires 'moreutils'
-{ knife node list && jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json } | ifne tee ~/.chef_hosts.new > /dev/null
+# NOTE: requires 'moreutils' and 'coreutils' (for gtimeout)
+
+{ gtimeout 2m knife node list 2>/dev/null && jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json } | ifne tee ~/.chef_hosts.new > /dev/null
+# replace if size greater than 0
 [[ -s ~/.chef_hosts.new ]] && mv ~/.chef_hosts.new ~/.chef_hosts
+# cleanup
+[[ -e ~/.chef_hosts.new ]] && rm ~/.chef_hosts.new
