4c2da116456cc158c5b203aa6715618dc95e2487
Daniel Kertesz <daniel@spatof.org>
2017-06-19 12:41:16 +0100

    just use awk and bypass 'wc'; minor fix

--- bin/zsh_chef_completion.sh
+++ bin/zsh_chef_completion.sh
@@ -2,8 +2,10 @@
 # Refresh zsh hostname completion
 # NOTE: requires 'moreutils' and 'coreutils' (for gtimeout)
 
-{ gtimeout 2m knife node list 2>/dev/null && jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json } | ifne tee ~/.chef_hosts.new > /dev/null
-# replace if size greater than 0
-[[ -s ~/.chef_hosts.new ]] && mv ~/.chef_hosts.new ~/.chef_hosts
-# cleanup
-[[ -e ~/.chef_hosts.new ]] && rm ~/.chef_hosts.new
+gtimeout 2m knife node list 2>/dev/null > /tmp/chef_hosts.new
+if [[ $(awk 'END { print NR }' /tmp/chef_hosts.new) -lt 5 ]]; then
+    exit 1
+fi
+
+jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json >> /tmp/chef_hosts.new
+mv /tmp/chef_hosts.new ~/.chef_hosts
