93cb1eed2d3e77501e42833dbfab7d3616c02bb9
Daniel Kertesz <daniel@spatof.org>
2018-07-02 11:15:47 +0100

    hyp data bags are gone

--- bin/zsh_chef_completion.sh
+++ bin/zsh_chef_completion.sh
@@ -2,10 +2,10 @@
 # Refresh zsh hostname completion
 # NOTE: requires 'moreutils' and 'coreutils' (for gtimeout)
 
-gtimeout 2m knife node list 2>/dev/null > /tmp/chef_hosts.new
-if [[ $(awk 'END { print NR }' /tmp/chef_hosts.new) -lt 5 ]]; then
-    exit 1
+if ~/bin/vpn_status >/dev/null; then
+    gtimeout 2m knife node list 2>/dev/null > /tmp/chef_hosts.new
+    if [[ $(awk 'END { print NR }' /tmp/chef_hosts.new) -lt 5 ]]; then
+	exit 1
+    fi
+    mv /tmp/chef_hosts.new ~/.chef_hosts
 fi
-
-jq -r .name ~/src/chef-repo/data_bags/hypervisors/*.json >> /tmp/chef_hosts.new
-mv /tmp/chef_hosts.new ~/.chef_hosts
