1434495b39cdda96a410a4b1ff0036bd897522ec
Daniel Kertesz <daniel@spatof.org>
2019-10-08 19:41:48 +0100

    emacs: add a sample dir-locals.el file

---
+++ emacs/dir-locals-sample.el
@@ -0,0 +1,3 @@
+;; see also https://www.gnu.org/software/emacs/manual/html_node/emacs/Directory-Variables.html
+((nginx-mode . ((nginx-indent-level . 2)))
+ (web-mode . ((web-mode-markup-indent-offset . 2))))
