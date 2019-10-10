c932e64386e6107d91c5481fe583f9aef1a9a211
Daniel Kertesz <daniel@spatof.org>
2019-10-10 14:25:09 +0100

    verbose errors

--- bin/zsh_chef_completion.sh
+++ bin/zsh_chef_completion.sh
@@ -3,9 +3,10 @@
 # NOTE: requires 'moreutils' and 'coreutils' (for gtimeout)
 
 if ~/bin/vpn_status >/dev/null; then
-    gtimeout 2m knife node list 2>/dev/null > /tmp/chef_hosts.new
+    # gtimeout 2m knife node list 2>/dev/null > /tmp/chef_hosts.new
+    gtimeout 2m knife node list > /tmp/chef_hosts.new
     if [[ $(awk 'END { print NR }' /tmp/chef_hosts.new) -lt 5 ]]; then
-	exit 1
+		exit 1
     fi
     mv /tmp/chef_hosts.new ~/.chef_hosts
 fi
