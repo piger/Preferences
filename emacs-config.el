8bb6e597f89033974baa34d611fe79a2b5b5235d
Daniel Kertesz <daniel@spatof.org>
2020-02-06 16:14:24 +0000

    emacs: treemacs

--- emacs-config.el
+++ emacs-config.el
@@ -1990,6 +1990,17 @@ becomes
 (use-package httpcode
   :commands hc)
 
+(use-package treemacs
+  :ensure t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :defer t)
+
+(use-package treemacs-projectile
+  :ensure t
+  :after treemacs projectile)
+
 ;; Aliases
 (defalias 'qrr 'query-replace-regexp)
 
