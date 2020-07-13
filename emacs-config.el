8ee84ac0dc56ed4c1ff1192f694ba8d33716ac2c
Daniel Kertesz <daniel@spatof.org>
2020-07-13 13:02:31 +0100

    emacs: disable transparency

--- emacs-config.el
+++ emacs-config.el
@@ -263,7 +263,7 @@
 
 ;;; transparency
 ;; (set-frame-parameter (selected-frame) 'alpha '(95 80))
-(add-to-list 'default-frame-alist '(alpha 95 90))
+;; (add-to-list 'default-frame-alist '(alpha 95 90))
 
 ;; Save clipboard strings into kill ring before replacing them.
 ;; When one selects something in another program to paste it into Emacs,
