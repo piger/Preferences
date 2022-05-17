41e93104be4d885b0f10f0ecbae175b79bb97d98
Daniel Kertesz <daniel@spatof.org>
2022-05-17 11:31:51 +0100

    zsh: remove hardcoded path

--- bin/zsh_chef_completion.sh
+++ bin/zsh_chef_completion.sh
@@ -2,7 +2,7 @@
 # Refresh zsh hostname completion
 # NOTE: requires 'moreutils' and 'coreutils' (for gtimeout)
 
-if ~/bin/vpn_status >/dev/null; then
+if vpn_status >/dev/null; then
     # gtimeout 2m knife node list 2>/dev/null > /tmp/chef_hosts.new
     gtimeout 2m knife node list > /tmp/chef_hosts.new
     if [[ $(awk 'END { print NR }' /tmp/chef_hosts.new) -lt 5 ]]; then
