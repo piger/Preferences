bbfb22616a58fdc0a48454e52f2aa1e0a8ae4c2e
Daniel Kertesz <daniel@spatof.org>
2022-09-21 22:18:26 +0100

    misc fixes for install_go... that I forgot to commit

--- bin/install_go.sh
+++ bin/install_go.sh
@@ -6,11 +6,15 @@ set -e
 which pv >/dev/null || exit
 
 ARCH="$(uname -m)"
+OS="$(uname -s | tr '[A-Z]' '[a-z]')"
 
 if [[ $ARCH == "x86_64" ]]; then
     ARCH="amd64"
+elif [[ $ARCH == "armv7l" ]]; then
+    ARCH="armv6l" # 32bit rpi4
 fi
 
+# Get the latest version available; doesn't matter here that we're grepping for "darwin-arm64"!
 VERSION=$(curl -sSL https://go.dev/dl/ | perl -nl -e '/go(\d+\.\d+)(\.\d+)?\.darwin-arm64\.tar\.gz/ && print "$1$2\n"' | head -n1)
 
 read -p "Latest version is ${VERSION}; continue? [yn] " -n 1 -r
@@ -18,17 +22,19 @@ echo
 
 [[ $REPLY =~ ^[Yy]$ ]] || { echo "ok, nevermind"; exit; }
 
-echo "Downloading go ${VERSION}: https://go.dev/dl/go${VERSION}.darwin-${ARCH}.tar.gz"
-curl -# -SL -O "https://go.dev/dl/go${VERSION}.darwin-${ARCH}.tar.gz"
+echo "Downloading go ${VERSION}: https://go.dev/dl/go${VERSION}.${OS}-${ARCH}.tar.gz"
+curl -# -SL -O "https://go.dev/dl/go${VERSION}.${OS}-${ARCH}.tar.gz"
 
 echo "Extracting go $VERSION in /opt/go"
 sudo rm -rf /opt/go
-pv "go${VERSION}.darwin-${ARCH}.tar.gz" | sudo tar -C /opt -xzf -
-rm -f "go${VERSION}.darwin-${ARCH}.tar.gz"
+pv "go${VERSION}.${OS}-${ARCH}.tar.gz" | sudo tar -C /opt -xzf -
+rm -f "go${VERSION}.${OS}-${ARCH}.tar.gz"
 
-echo "Setting up /etc/paths.d"
-if [[ ! -e /etc/paths.d/go ]]; then
-    echo "/opt/go/bin" | sudo tee /etc/paths.d/go
+if [[ "$OS" == "darwin" ]]; then
+    echo "Setting up /etc/paths.d"
+    if [[ ! -e /etc/paths.d/go ]]; then
+        echo "/opt/go/bin" | sudo tee /etc/paths.d/go
+    fi
 fi
 
 echo "Fixing permissions"
