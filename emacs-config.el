f46eb32560332173eeb2d73dfce5d3c0da9e1019
Daniel Kertesz <daniel@spatof.org>
2023-05-01 13:38:18 +0100

    emacs: force loading ivy-rich after ivy and counsel

--- emacs-config.el
+++ emacs-config.el
@@ -993,7 +993,7 @@ becomes
 
 ;; fancy descriptions in M-x
 (use-package ivy-rich
-  :ensure t
+  :after (ivy counsel)
   :config
   (setcdr (assq t ivy-format-functions-alist) #'ivy-format-function-line)
   (ivy-rich-mode 1))
