6b6d7ce1a22386b4a98e31eb3f41ec34ddc2bbea
Daniel Kertesz <daniel@spatof.org>
2024-03-05 21:49:10 +0000

    python: streamline REPL configuration
    - tab completion is now native
    - prompt color don't work

--- pythonrc.py
+++ pythonrc.py
@@ -1,7 +1,13 @@
+# some common imports
+import os
 import sys
-import readline
-import rlcompleter
+import json
 from pprint import pprint
+
+
+# use pprint() to print expressions
+# https://docs.python.org/3/library/sys.html#sys.displayhook
+sys.displayhook = pprint
 
 # Import `see()` (better alternative to `dir()`) when it's available.
 # https://ljcooke.github.io/see/
@@ -9,19 +15,3 @@ try:
     from see import see
 except ImportError:
     pass
-
-# Configure the TAB keyboard key to execute the completion function.
-if 'libedit' in readline.__doc__:
-    readline.parse_and_bind("bind ^I rl_complete")
-else:
-    readline.parse_and_bind("tab: complete")
-
-# prompt colors (in a format that doesn't mess with the REPL)
-# https://stackoverflow.com/a/10953944
-# https://stackoverflow.com/a/9468954
-sys.ps1 = '\001\033[96m\002>>> \001\033[0m\002'
-sys.ps2 = '\001\033[96m\002... \001\033[0m\002'
-
-# https://docs.python.org/2/library/sys.html#sys.displayhook
-# use pprint() to print expressions
-sys.displayhook = pprint
