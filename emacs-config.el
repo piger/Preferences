2379d4ae58b03f63cc3302ee97919534913d4b72
Daniel Kertesz <daniel@spatof.org>
2024-03-12 00:21:39 +0000

    emacs: give another try to electric pairs mode

--- emacs-config.el
+++ emacs-config.el
@@ -1008,9 +1008,10 @@ becomes
 
 (use-package prog-mode
   :ensure nil
-  :hook (prog-mode . bug-reference-mode))
-  ;; :config
-  ;; (electric-pair-mode))
+  :hook (prog-mode . bug-reference-mode)
+  :config
+  (setq electric-pair-skip-self t)
+  (electric-pair-mode))
 
 ;; https://oylenshpeegul.gitlab.io/blog/posts/20230129/
 ;; Add a contextual menu to launch 'git-link' by right-clicking on a line.
