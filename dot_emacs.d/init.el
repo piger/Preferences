3df6b21e07a6152b2da71c77ea7fd125cce207cb
Daniel Kertesz <daniel@spatof.org>
2026-07-21 01:17:21 +0200

    emacs: re-enable crux
    my replacement was half-assed and didn't include all the needed code. Better to
    just use crux.

--- dot_emacs.d/init.el
+++ dot_emacs.d/init.el
@@ -1864,38 +1864,11 @@ becomes
   :commands whitespace-cleanup-mode
   :diminish 'whitespace-cleanup-mode)
 
-;; misc utilities
+;; misc utilities...
+;; of which I only use one.
 (use-package crux
-  :disabled ;; see below
   :config
-  (global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
-  (global-set-key (kbd "C-c o") #'crux-open-with))
-
-;; this is the only function from crux that I've ever used.
-(defun crux-move-beginning-of-line (arg)
-  "Move point back to indentation of beginning of line.
-
-Move point to the first non-whitespace character on this line.
-If point is already there, move to the beginning of the line.
-Effectively toggle between the first non-whitespace character and
-the beginning of the line.
-
-If ARG is not nil or 1, move forward ARG - 1 lines first.  If
-point reaches the beginning or end of the buffer, stop there."
-  (interactive "^p")
-  (setq arg (or arg 1))
-
-  ;; Move lines first
-  (when (/= arg 1)
-    (let ((line-move-visual nil))
-      (forward-line (1- arg))))
-
-  (let ((orig-point (point)))
-    (crux-move-to-mode-line-start)
-    (when (= orig-point (point))
-      (move-beginning-of-line 1))))
-
-(global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line)
+  (global-set-key [remap move-beginning-of-line] #'crux-move-beginning-of-line))
 
 (use-package editorconfig
   :diminish
